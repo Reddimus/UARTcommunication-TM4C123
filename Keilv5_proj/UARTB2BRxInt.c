9a3f141c138d89aa87fa9f7a51666cb339f27480
Reddimus <Kevi.M.CECS@outlook.com>
2023-10-04 16:57:52 -0700

    Added multiple colors

--- Keilv5_proj/UARTB2BRxInt.c
+++ Keilv5_proj/UARTB2BRxInt.c
@@ -5,10 +5,9 @@
 // Ground connected ground in the USB cable
 #include "tm4c123gh6pm.h"
 #include "UART.h"
-#include <stdbool.h>  // for C boolean data type
 
 // bit address definition for port data registers
-#define LED (*((volatile uint32_t *)0x40025008))  // use onboard RED LED: PF1
+#define LED (*((volatile uint32_t *)0x40025038))  // use onboard RED LED: PF3-PF1
 
 ////////// Constants //////////  
 // Color    LED(s) PortF
@@ -21,13 +20,19 @@
 // pink     R-B    0x06
 // Cran     -GB    0x0C
 
+#define RGB		0x0E
+
 #define DARK	0x00
 #define RED		0x02
+#define BLUE	0x04
+#define GREEN	0x08
+
 #define SW1		0x10
 #define SW2		0x01
 
 #define NVIC_EN0_PORTF	0x40000000
 
+#define COLORS_SIZE 4
 
 extern void DisableInterrupts(void);
 extern void EnableInterrupts(void);  
@@ -35,12 +40,16 @@ extern void WaitForInterrupt(void);
 
 void GPIO_PortF_Init(void);
 
+static const uint8_t colors[COLORS_SIZE] = {DARK, RED, BLUE, GREEN};
+volatile uint8_t colorsIdx;
+
 int main(void) {	
 	DisableInterrupts();	
 	UART1_Init(true,false);	// initialize UART with RX interrupt
 	GPIO_PortF_Init();		// initialize port F
 	EnableInterrupts();		// needed for TExaS
 	LED = DARK;
+	colorsIdx = 0;
 	
 	while(1) {
 		WaitForInterrupt();
@@ -52,13 +61,13 @@ void GPIO_PortF_Init(void) {
 	while ((SYSCTL_RCGC2_R &= SYSCTL_RCGC2_GPIOF)==0);
 
 	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;					// 2) unlock PortF PF0  
-	GPIO_PORTF_CR_R |= 0x13;							//		allow changes to PF4,1,0       
-	GPIO_PORTF_DIR_R |= 0x02;							// (c) make LEDs outputs
-	GPIO_PORTF_DIR_R &= ~0x11;
-	GPIO_PORTF_AFSEL_R &= ~0x13;						//		disable alt funct 
-	GPIO_PORTF_DEN_R |= 0x13;							//		enable digital   
-	GPIO_PORTF_PCTL_R &= ~0x000F00FF;					//		configure as GPIO
-	GPIO_PORTF_AMSEL_R &= ~0x13;						//		disable analog functionality on PF
+	GPIO_PORTF_CR_R |= 0x1F;							// allow changes to PF4-0       
+	GPIO_PORTF_DIR_R |= 0x0E;							//		PF3,2,1 output
+	GPIO_PORTF_DIR_R &= ~0x11;							//		PF4,0 input
+	GPIO_PORTF_AFSEL_R &= ~0x1F;						//		disable alt funct on PF4-0
+	GPIO_PORTF_DEN_R |= 0x1F;							//		enable digital I/O on PF4-0
+	GPIO_PORTF_PCTL_R &= ~0x000FFFFF;					// 		configure PF4-0 as GPIO
+	GPIO_PORTF_AMSEL_R &= ~0x1F;						//		disable analog functionality on PF4-0
 	GPIO_PORTF_PUR_R |= 0x11;							//		enable weak pull-up on PF0,4
 
 	GPIO_PORTF_IS_R &= ~0x11;							// (d) PF0,4 is edge-sensitive
@@ -76,20 +85,22 @@ void GPIOPortF_Handler(void) {
 	
 	if(GPIO_PORTF_RIS_R & SW2) {
 		GPIO_PORTF_ICR_R = SW2;
-		LED ^= RED;
+		colorsIdx = (colorsIdx+1) % COLORS_SIZE;
+		LED = colors[colorsIdx];
 	}
 	
 	if(GPIO_PORTF_RIS_R & SW1) {
 		GPIO_PORTF_ICR_R = SW1;
-		UART1_OutChar(LED);		
+		UART1_OutChar(colorsIdx);
 	}
 }
 
 // Take care of Rx interrupt and ignore Tx interrupt
 void UART1_Handler(void) {
 	if(UART1_RIS_R&UART_RIS_RXRIS) {		// received one item
-		if ((UART1_FR_R&UART_FR_RXFE) == 0)
-			LED = UART1_DR_R&RED;				//led_on = UART1_DR_R&RED;
+		if ((UART1_FR_R&UART_FR_RXFE) == 0) {
+			LED = colors[UART1_DR_R & (COLORS_SIZE - 1)];	// read transmitted colors index then convert to color
+		}
 		UART1_ICR_R = UART_ICR_RXIC;		// acknowledge RX FIFO
 	}
 }
